350e50164380f5b0bd1c2c0ad36d7e32de61d85a
dzha0064 <dzha0064@student.monash.edu>
2026-08-28 03:22:02 +1000

    feat: add navigation and routing for US1

---
+++ Curbi/src/views/TaskView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Your alternative task</h1>
+
+    <p>Take a short walk for 5 minutes without checking your symptoms.</p>
+
+    <RouterLink to="/complete">
+      <button>I've completed this task</button>
+    </RouterLink>
+  </div>
+</template>
